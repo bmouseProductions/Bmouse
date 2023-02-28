67e4355c049eed89985e7fb3cce024fa0bf23f1e
beno1234 <benolopesdias@gmail.com>
2023-02-28 12:24:58 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
+/* // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 })
+ */
+
+export default defineConfig({
+  plugins: [atomicPlugin(allAtomicCSSPaths)],
+  resolve: {
+    alias: [{ find: "@", replacement: "/src" }],
+  },
+  server: {
+    fs: {
+      allow: [".."],
+    },
+  },
+});
