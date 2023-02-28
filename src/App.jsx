f56d244913d898c53cfe12c02b1d6d0c518f0e82
beno1234 <benolopesdias@gmail.com>
2023-02-28 14:21:32 -0300

    teste4

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Navbar, Footer } from "./components";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/home/Home";
 import Servicos from "./Pages/servicos/Servicos";
+import GalaxyBackground from "react-animated-stars";
 import Studio from "./Pages/studio/Studio";
 import Portfolio from "./Pages/portfolio/Portfolio";
 import Blog from "./Pages/blog/Blog";
@@ -10,25 +11,27 @@ import Blog from "./Pages/blog/Blog";
 const App = () => {
   return (
     <BrowserRouter>
-      <div className="bg-primary w-full overflow-hidden">
-        <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-          <div className={`${styles.boxWidth}`}>
-            <Navbar />
+      <GalaxyBackground starCount={50} key={1}>
+        <div className="bg-primary w-full overflow-hidden">
+          <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+            <div className={`${styles.boxWidth}`}>
+              <Navbar />
+            </div>
+          </div>
+          <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+            <div className={`${styles.boxWidth}`}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/servicos" element={<Servicos />} />
+                <Route path="/studiosbmouse" element={<Studio />} />
+                <Route path="/portfolio" element={<Portfolio />} />
+                <Route path="/blog" element={<Blog />} />
+              </Routes>
+              <Footer />
+            </div>
           </div>
         </div>
-        <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-          <div className={`${styles.boxWidth}`}>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/servicos" element={<Servicos />} />
-              <Route path="/studiosbmouse" element={<Studio />} />
-              <Route path="/portfolio" element={<Portfolio />} />
-              <Route path="/blog" element={<Blog />} />
-            </Routes>
-            <Footer />
-          </div>
-        </div>
-      </div>
+      </GalaxyBackground>
     </BrowserRouter>
   );
 };
